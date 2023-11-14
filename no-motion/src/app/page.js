69513c73d1caf44dfe617fe351fde7b89a8ab3cc
Miguel-AngelGarcia <mgar574@wgu.edu>
2023-11-14 15:50:38 -0500

    Added projects section.

--- no-motion/src/app/page.js
+++ no-motion/src/app/page.js
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero/Hero";
 import Work from "@/components/Work";
 import Stack from "@/components/Stack/Stack";
 import { Footer } from "@/components/Footer";
+import { Projects } from "@/components/Projects";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <div className="sect-wrapper">
         <Hero />
         <Work />
+        <Projects />
         <Stack />
       </div>
 
---
+++ no-motion/src/components/Projects.js
@@ -0,0 +1,67 @@
+import React from "react";
+import Image from "next/image";
+
+export const Projects = () => {
+  const projects = [
+    {
+      id: 1,
+      projName: "Real Estate Predictor",
+      tags: [
+        { id: 1, tagName: "Machine Learning" },
+        { id: 2, tagName: "Full Stack" },
+        { id: 3, tagName: "REST API" },
+        { id: 4, tagName: "SQLite" },
+        { id: 5, tagName: "Pandas" },
+      ],
+      img: "/ml.png",
+    },
+    {
+      id: 2,
+      projName: "Whats For Dinner",
+      tags: [
+        { id: 1, tagName: "Responsive" },
+        { id: 2, tagName: "SQL Database" },
+        { id: 3, tagName: "Java Spring Boot" },
+      ],
+      img: "/food.png",
+    },
+    {
+      id: 3,
+      projName: "Photography Showcase",
+      tags: [
+        { id: 1, tagName: "Motion" },
+        { id: 2, tagName: "JavaScript" },
+        { id: 3, tagName: "CSS" },
+      ],
+      img: "/photo.png",
+    },
+    {
+      id: 4,
+      projName: "Canvas",
+      tags: [
+        { id: 1, tagName: "Game" },
+        { id: 2, tagName: "Canvas" },
+        { id: 3, tagName: "JavaScript" },
+      ],
+      img: "/game.png",
+    },
+  ];
+  return (
+    <section className="projects-wrapper">
+      <div className="projects-cont">
+        {projects.map((project) => (
+          <div className={`each-proj proj-${project.id}`} key={project.id}>
+            <Image src={project.img} alt="test" priority={true} fill />
+            <div className="proj-tag-cont">
+              {project.tags.map((tag) => (
+                <div key={tag.id} className="tag-cont outline-cs">
+                  <p className="pro-tag">{tag.tagName}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
